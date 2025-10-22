9a8af96553df6c17a69d40c8c23fefa6df6537dc
Adarsh P Thomson <131739584+Adarsh-P-Thomson@users.noreply.github.com>
2025-10-22 14:58:13 +0000

    next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Since you are using a custom domain, basePath should be empty.
+  // `images.unoptimized = true` is required for static exports.
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  trailingSlash: true,
-}
+};
 
-export default nextConfig
+export default nextConfig;
